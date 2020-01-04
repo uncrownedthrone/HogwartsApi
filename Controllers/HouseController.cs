acf0dbdffebb2dd2cdcdc02199b7f692deb0b5c8
Justin Swiger <justin.swiger@me.com>
2020-01-04 16:12:52 -0500

    put endpoint working

--- Controllers/HouseController.cs
+++ Controllers/HouseController.cs
@@ -14,6 +14,22 @@ namespace HogwartsApi.Controllers
       var db = new DatabaseContext();
       return Ok(db.Houses.OrderBy(house => house.HouseName));
     }
+
+    [HttpGet("{id}")]
+    public ActionResult GetOneHouse(int id)
+    {
+      var db = new DatabaseContext();
+      var house = db.Houses.FirstOrDefault(ho => ho.Id == id);
+      if (house == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        return Ok(house);
+      }
+    }
+
     [HttpPost]
     public ActionResult CreateHouse(House house)
     {
@@ -21,6 +37,24 @@ namespace HogwartsApi.Controllers
       db.Houses.Add(house);
       db.SaveChanges();
       return Ok(house);
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult UpdateHouse(House house)
+    {
+      var db = new DatabaseContext();
+      var prevHouse = db.Houses.FirstOrDefault(ho => ho.Id == house.Id);
+      if (prevHouse == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        prevHouse.HouseName = house.HouseName;
+        prevHouse.HouseColor = house.HouseColor;
+        db.SaveChanges();
+        return Ok(prevHouse);
+      }
     }
   }
 }
